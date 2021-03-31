0668631a83a4bf6b1a03c1272b491b824509fa59
chayon11945 <chayonfolk@gmail.com>
2021-04-01 06:41:40 +0700

    2 files modified

--- index.js
+++ index.js
@@ -27,8 +27,7 @@ const code = document.getElementById("code")
 const friendShip = document.getElementById("friendShip")
 
 async function main(){
-  // ...
-  getUserProfile()
+
   // Initialize LIFF app)
   await liff.init({liffId:"1655817271-wazQAYM0"})
   
@@ -37,13 +36,59 @@ async function main(){
   case "android": body.style.backgroundColor = "#d1f5d3"; break
   case "ios": body.style.backgroundColor = "#eeeeee"; break
 }
+   // ...
+  if (!liff.isInClient()) {
+    if (liff.isLoggedIn()) {
+      btnLogIn.style.display = "none"
+      btnLogOut.style.display = "block"
+      getUserProfile()
+    } else {
+      btnLogIn.style.display = "block"
+      btnLogOut.style.display = "none"
+    }
+  } else {
+    getUserProfile()
+  }
 }
+
+main()
+
 async function getUserProfile() {
   const profile = await liff.getProfile()
   pictureUrl.src = profile.pictureUrl
   userId.innerHTML = "<b>userId:</b> " + profile.userId
   statusMessage.innerHTML = "<b>statusMessage:</b> " + profile.statusMessage
   displayName.innerHTML = "<b>displayName:</b> " + profile.displayName
-    email.innerHTML = "<b>email:</b> " + liff.getDecodedIDToken().email
+  email.innerHTML = "<b>email:</b> " + liff.getDecodedIDToken().email
 }
-main()
+
+
+
+  if (!liff.isInClient()) {
+    btnLogIn.style.display = "block"
+    btnLogOut.style.display = "block"
+
+}
+
+btnLogIn.onclick = () => {
+  liff.login()
+}
+
+btnLogOut.onclick = () => {
+  liff.logout()
+  window.location.reload()
+}
+async function sendMsg() {
+  if (liff.getContext().type !== "none" && liff.getContext().type !== "external") {
+    await liff.sendMessages([
+      {
+        "type": "text",
+        "text": "This message was sent by sendMessages()"
+      }
+    ])
+    alert("Message sent")
+  }
+}
+btnSend.onclick = () => {
+  sendMsg()
+}
